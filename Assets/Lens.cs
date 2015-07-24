9caf8657600868e78b7f330235083fc6c4d8ce2d
Chi-kwan Chan <chanc@email.arizona.edu>
2015-07-23 23:20:48 -0400

    Programmatically add the "Schwarzschild" script to "Main Camera"

--- Assets/Lens.cs
+++ Assets/Lens.cs
@@ -5,7 +5,10 @@ public class Lens : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		var effect = GetComponent<Schwarzschild> ();
+		if (effect == null) {
+			effect = gameObject.AddComponent<Schwarzschild> ();
+		}
 	}
 	
 	// Update is called once per frame
